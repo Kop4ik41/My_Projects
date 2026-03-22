4b77489a3e017a29947a95a90dbb50e7897a4f43
Kop4ik41 <kostyauvarov1@gmail.com>
2026-03-22 12:35:15 +0300

    Delete Lab1CDISpring directory

--- Lab1CDISpring/src/main/java/ru/rut_miit/lab1cdispring/SortingMethods/ICollectionSorter.java
+++
@@ -1,8 +0,0 @@
-package ru.rut_miit.lab1cdispring.SortingMethods;
-
-import java.util.List;
-
-
-public interface ICollectionSorter {
-    <T extends Comparable<? super T>> void sort(List<T> list);
-}
